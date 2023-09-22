ca4f066bc02dba35b42f2a2f527c217f38c0628b
sachindev99 <97806092+sachindev99@users.noreply.github.com>
2023-09-22 12:59:06 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//calling multiple functions on page load
+window.onload = function () {
+    playersDice();
+}
+
+function playersDice() {
+    var player1DiceNumber = randomNumberGenerator();
+    var player2DiceNumber = randomNumberGenerator();
+    document.querySelector(".img1").setAttribute("src", "./images/dice"+player1DiceNumber+".png");
+    document.querySelector(".img2").setAttribute("src", "./images/dice"+player2DiceNumber+".png");
+
+    if(player1DiceNumber>player2DiceNumber){
+       document.querySelector("h1").innerHTML = "🚩 Player 1 wins";
+    }
+
+    else if(player2DiceNumber>player1DiceNumber){
+        document.querySelector("h1").innerHTML= "Player 2 wins 🚩";
+    }
+
+    else{
+        document.querySelector("h1").innerHTML= "It's a draw!";
+    }
+    
+}
+
+
+function randomNumberGenerator() {
+    var createRandomNum = Math.floor(Math.random() * 6) + 1;
+    return createRandomNum;
+}
